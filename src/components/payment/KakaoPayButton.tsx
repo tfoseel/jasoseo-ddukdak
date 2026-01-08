d32c3319ed52cff6cf543fd1edcce0709d5b7dba
tfoseel <tfoseel@gmail.com>
2026-01-09 08:34:17 +0900

    💄카카오 심사 준비

--- src/components/payment/KakaoPayButton.tsx
+++ src/components/payment/KakaoPayButton.tsx
@@ -62,9 +62,11 @@ export function KakaoPayButton({ itemName, amount, userId, onSuccess, onError }:
                 </>
             ) : (
                 <>
-                    <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M12 3C6.48 3 2 6.93 2 11.75c0 2.89 1.86 5.43 4.65 6.89-.06.52-.4 2.04-.46 2.37 0 0-.03.25.13.35.16.09.35.01.35.01.46-.06 5.31-3.47 6.16-4.05.39.05.79.08 1.17.08 5.52 0 10-3.93 10-8.75S17.52 3 12 3z" />
-                    </svg>
+                    <img
+                        src="/images/kakaopay-badge.png"
+                        alt="KakaoPay"
+                        className="h-7 w-auto mr-2"
+                    />
                     카카오페이로 결제하기
                 </>
             )}
